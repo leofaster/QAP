eb101fc5c864e1a50e1559b15607f69f63854dcd
Leopoldo Pimentel <leofaster@gmail.com>
2015-05-04 00:59:15 -0430

    Cambios Finales primera entrega

--- LocalSearch/qap.py
+++ LocalSearch/qap.py
@@ -19,18 +19,118 @@ def generate_vicinity_random(size):
         rand = random.choice(v)
         v.remove(rand)
         v.append(rand)
+
     return v
+
+
+def permute_min_local_random(vicinity, position):
+    swapPosition = random.choice(range(position, len(vicinity)))
+    tmp = vicinity[position]
+    vicinity[position] = vicinity[swapPosition]
+    vicinity[swapPosition] = tmp
+
+    return vicinity
+
+
+def permute_min_local_next(vicinity, position, swich):
+    tmp = vicinity[position]
+    vicinity[position] = vicinity[swich]
+    vicinity[swich] = tmp
+
+    return vicinity
 
 
 def permute_vicinity(vicinity, first, second):
     temp = vicinity[first]
     vicinity[first] = vicinity[second]
     vicinity[second] = temp
+
     return vicinity
 
 
+def obtain_min_random(vicinity, vicinityValue, size, flow, distance, maxtime):
+    position = 0
+    laps = 0
+    while time.clock() < maxtime:
+        laps += 1
+        newVicinity = permute_min_local_random(vicinity, position)
+        newVicinityValue = val(size, newVicinity, flow, distance)
+        if vicinityValue > newVicinityValue:
+            vicinity = newVicinity
+            vicinityValue = newVicinityValue
+            position += 1
+
+    print "Mejor valor: ", vicinityValue
+    print "Tiempo de corrida: ", time.clock()
+    print "Iteraciones: ", laps
+
+
+def obtain_min_random_next(
+    vicinity,
+    vicinityValue,
+    size,
+    flow,
+    distance,
+    maxtime
+):
+
+    position = 0
+    sw = 1
+    laps = 0
+    while time.clock() < maxtime and position < size:
+        laps += 1
+        newVicinity = permute_min_local_next(vicinity, position, sw)
+        newVicinityValue = val(size, newVicinity, flow, distance)
+
+        if vicinityValue > newVicinityValue:
+            vicinity = newVicinity
+            vicinityValue = newVicinityValue
+            position += 1
+            sw += 1
+        else:
+            if sw + 1 >= size:
+                if (position + 1) >= size:
+                    position = 0
+                    sw = 1
+                else:
+                    position += 1
+                    if position + 1 >= size:
+                        sw = 0
+                    else:
+                        sw = position + 1
+            else:
+                sw += 1
+
+    print "Mejor valor: ", vicinityValue
+    print "Tiempo de corrida: ", time.clock()
+    print "Iteraciones: ", laps
+
+
+def obtain_min_force(
+    vicinity, vicinityValue, size, flow, distance, maxtime
+):
+    i = 0
+    j = size-1
+    laps = 0
+    while time.clock() < maxtime and j > 0 and i < size:
+        laps += 1
+        newVicinity = permute_vicinity(vicinity, i, j)
+        newVicinityValue = val(size, newVicinity, flow, distance)
+        if vicinityValue > newVicinityValue:
+            vicinity = newVicinity
+            vicinityValue = newVicinityValue
+        if j <= 1:
+            i += 1
+            j = size - 1
+        else:
+            j -= 1
+
+    print "Mejor valor: ", vicinityValue
+    print "Tiempo de corrida: ", time.clock()
+    print "Iteraciones: ", laps
+
+
 def main(argv):
-        # my code here
     if len(argv) > 1:
         f = open(sys.argv[1], 'r')
     else:
@@ -38,49 +138,53 @@ def main(argv):
     if len(argv) > 2:
         maxtime = float(sys.argv[2])
     else:
-        maxtime = 50
+        maxtime = 10
 
     size = int(f.readline().strip())
-    f.readline()
 
-    flow = [[0 for x in range(size)] for x in range(size)]
-    distance = [[0 for x in range(size)] for x in range(size)]
+    f.readline()  # We read and leave this line
+
+    # Initializate the variables
     vicinity = [0 for x in range(size)]
-    for counter, line in enumerate(f):
-        bufer = line.split()
-        if bufer == '':
+    i = 0
+    matrix = []
+    for line in f:
+        y = line.strip()
+        if y == '':
             continue
-        if counter < size+1:
-            # print "primera Matriz"
-            for col, number in enumerate(bufer):
-                flow[counter][col] = number
-        else:
-            # print "Segunda Matriz"
-            for col, number in enumerate(bufer):
-                distance[counter-(size+1)][col] = number
+        for z in y.split():
+            if i % size == 0:
+                matrix.append([])
+            matrix[-1].append(int(z))
+            i += 1
+    assert i == 2 * size * size
+    flow = matrix[:size]
+    distance = matrix[size:]
 
-        # print bufer
     vicinity = generate_vicinity_random(size)
-    # vicinity = [7,5,12,2,1,3,9,11,10,6,8,4]
-    # for n     in range(len(vicinity)):
-        # vicinity[n] = vicinity[n] -1
-    # print vicinity
-    # print val(size, vicinity, flow, distance)
     vicinityValue = val(size, vicinity, flow, distance)
-    while time.clock() < maxtime:
-        # print time.clock()
-        for i in range(size):
-            for counter, j in enumerate(range(size)):
-                if counter == 0:
-                    j+=1
-                newVicinity = permute_vicinity(vicinity, i, j)
-                newVicinityValue = val(size, newVicinity, flow, distance)
-                print newVicinity, newVicinityValue
-                if vicinityValue > newVicinityValue:
-                    vicinity = newVicinity
-                    vicinityValue = newVicinityValue
+    vicinityValueOLD = vicinityValue
+    print "Cual de las siguientes heuristicas quiere probar?"
+    print "0) Brute Force"
+    print "1) Local Search, random switch"
+    print "2) Local Search, next switch"
+    num = ''
+    while True:
+        try:
+            num = int(raw_input("Seleccione uno: "))
+            if int(num) in [0, 1, 2]:
+                break
+        except:
+            print 'Por favor un numero'
 
-    print "Mejor Vecindad Encontrada: " , vicinity
-    print "Mejor valor: ", vicinityValue
+    options = {
+        0: obtain_min_force,
+        1: obtain_min_random,
+        2: obtain_min_random_next,
+    }
+    print "Empezamos con valor: ", vicinityValueOLD
+    options[num](vicinity, vicinityValue, size, flow, distance, maxtime)
+
+
 if __name__ == "__main__":
     main(sys.argv)
